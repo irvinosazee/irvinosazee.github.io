91cd5fdfe7b5e36fc91d46f27540f1008a5b2a53
heiis-irvin <136273899+heiis-irvin@users.noreply.github.com>
2023-09-01 00:18:41 +0100

    Responsive Header

---
+++ Readme.md
@@ -0,0 +1,27 @@
+This is a Portfolio Project 
+    Author: Irvin
+
+    Web Tools: 
+        -Php
+        -Css
+        -Javascript
+
+    Sources: 
+        -Google Fonts :"https://fonts.google.com/specimen/Montserrat"
+        -Neumorphism.io : "https://neumorphism.io/#c60c0c",
+        -Wepik/Storyset : "https://storyset.com/",
+        -Undraw : "https://undraw.co/illustrations",
+        -Color palettes : "https://coolors.co/palettes/trending",
+        -Box Icons : "https://boxicons.com/".
+
+
+    Color palettes:
+        -federal-blue: #03045eff;
+        -marian-blue: #023e8aff;
+        -honolulu-blue: #0077b6ff;
+        -blue-green: #0096c7ff;
+        -pacific-cyan: #00b4d8ff;
+        -vivid-sky-blue: #48cae4ff;
+        -non-photo-blue: #90e0efff;
+        -non-photo-blue-2: #ade8f4ff;
+        -light-cyan: #caf0f8ff;
---
+++ pages/header.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="/styles/header.css">
+<script defer src="/scripts/header.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+<header>
+    <div class="header">
+        <span class="logo">IRVIN</span>
+        <nav class="nav">
+            <span><a href="">Home</a></span>
+            <span><a href="">About</a></span>
+            <span><a href="">Skills</a></span>
+            <span><a href="">Contact</a></span>
+            <span><button><img src="/assets/icons/sun-unactive.svg" alt="Light mode"></button></span>
+            <span><button><img src="/assets/icons/moon-unactive.svg" alt="Light mode"></button></span>
+        </nav>
+        <nav class="nav-p">
+            <span><button><img src="/assets/icons/menu.svg" alt="Light mode"></button></span>
+        </nav>
+    </div>
+</header>
