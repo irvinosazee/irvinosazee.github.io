2241fa844ea1903deaa45f16645dedbbb069fab2
heiis-irvin <136273899+heiis-irvin@users.noreply.github.com>
2023-09-02 15:50:05 +0100

    Skills

--- pages/skills.php
+++ pages/skills.php
@@ -10,7 +10,63 @@
             The skills, tools and technologies I am really good at:
             </p>
         </div>
-        <div class="cards">
+        <div class="cards ">
+            <div class="card">
+                <img class="card-img" src="/assets/Skills/assets/git.svg" alt="language logo">
+                <p>Git</p>
+            </div>
+            <div class="card">
+                <img class="card-img" src="/assets/Skills/assets/figma.svg" alt="language logo">
+                <p>Figma</p>
+            </div>
+            <div class="card">
+                <img class="card-img" src="/assets/Skills/assets/html.svg" alt="language logo">
+                <p>Html</p>
+            </div>
+            <div class="card">
+                <img class="card-img" src="/assets/Skills/assets/css.svg" alt="language logo">
+                <p>Css</p>
+            </div>
+            <div class="card">
+                <img class="card-img" src="/assets/Skills/assets/javascript.svg" alt="language logo">
+                <p>JavaScript</p>
+            </div>
+            <div class="card">
+                <img class="card-img" src="/assets/Skills/assets/nodejs.svg" alt="language logo">
+                <p>Node js</p>
+            </div>
+            <div class="card">
+                <img class="card-img" src="/assets/Skills/assets/php.svg" alt="language logo">
+                <p>Php</p>
+            </div>
+            <div class="card">
+                <img class="card-img" src="/assets/Skills/assets/tailwindcss.svg" alt="language logo">
+                <p>TailwindCss</p>
+            </div>
+            <div class="card">
+                <img class="card-img" src="/assets/Skills/assets/typescript.svg" alt="language logo">
+                <p>TypeScript</p>
+            </div>
+            <div class="card">
+                <img class="card-img" src="/assets/Skills/assets/angular.svg" alt="language logo">
+                <p>Angular</p>
+            </div>
+            <div class="card">
+                <img class="card-img" src="/assets/Skills/assets/python.svg" alt="language logo">
+                <p>Python</p>
+            </div>
+            <div class="card">
+                <img class="card-img" src="/assets/Skills/assets/dart.svg" alt="language logo">
+                <p>Dart</p>
+            </div>
+            <div class="card">
+                <img class="card-img" src="/assets/Skills/assets/flutter.svg" alt="language logo">
+                <p>Flutter</p>
+            </div>
+            <div class="card">
+                <img class="card-img" src="/assets/Skills/assets/firebase.svg" alt="language logo">
+                <p>Firebase</p>
+            </div>
         </div>
     </div>
 </section>
