b7cb46d3583780187fdd50faa72eba87905bc80d
heiis-irvin <136273899+heiis-irvin@users.noreply.github.com>
2023-09-03 14:03:30 +0100

    header mobile fix

--- scripts/header.js
+++ scripts/header.js
@@ -3,6 +3,11 @@ document.body.addEventListener('keydown' , event=>{
     console.log(event.key); 
     event.key = "Enter" ?  closePop() : null ;
 })
+
+
+document.querySelector(`.menu`).addEventListener('click' , ()=>{
+    document.querySelector(".menu").style.display = "flex" ? closePop() : null
+})
 // menubar functionalities 
 function closePop() {
 document.querySelector(".menu").style.display = "none";
